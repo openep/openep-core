0f1364b97b831a61a8c44f4be7feee22c307a73f
Steven Williams <steven.e.williams@kcl.ac.uk>
2021-01-29 09:53:07 +0000

    modified getMappingPointsWithinMesh.m to accept an image mask

--- getMappingPointsWithinMesh.m
+++ getMappingPointsWithinMesh.m
@@ -6,7 +6,8 @@ function iPoint = getMappingPointsWithinMesh( userdata, varargin )
 %   iPoint = getMappingPointsWithinMesh( userdata )
 % Where:
 %   userdata  - see importcarto_mem
-%   iPoint  - logical array list of valid points; indexes into userdata.electric
+%   iPoint  - logical array list of valid points; indexes into
+%             `userdata.electric`.
 %
 % GETMAPPINGPOINTSWITHINMESH accepts the following parameter-value pairs
 %   'tol' 0.1 | double
@@ -14,6 +15,9 @@ function iPoint = getMappingPointsWithinMesh( userdata, varargin )
 %         internal or external to the triangulation
 %   'plot'     {false}|true
 %       - Specify whether to plot the results
+%   'mask' {[]} | {Triangulation}
+%       - A Triangulation object describing a supplementary three
+%         dimensional mask to be used for filtering points
 %
 % GETMAPPINGPOINTSWITHINMESH Returns the indices of the mapping points
 % which are located internal to the mesh. The OpenEP function pointStatus.m
@@ -36,6 +40,32 @@ function iPoint = getMappingPointsWithinMesh( userdata, varargin )
 % code
 % ---------------------------------------------------------------
 
-[iPoint, ~] = pointStatus( userdata, varargin );
+nStandardArgs = 1; % UPDATE VALUE
+plot = false;
+tol = 0.1;
+mask = [];
+
+if nargin > nStandardArgs && ~isempty(varargin{1})
+    for i = 1:2:nargin-1
+        switch varargin{i}
+            case 'plot'
+                plot = varargin{i+1};
+            case 'tol'
+                tol = varargin{i+1};
+            case 'mask'
+                mask = varargin{i+1};
+        end
+    end
+end
+
+[iPoint, ~] = pointStatus( userdata, 'tol', tol, 'plot', plot);
+
+if ~isempty(mask)
+    % we need to find the points which are internal to the mask
+    FV.vertices = mask.Points;
+    FV.faces = mask.ConnectivityList;
+    QPTS = userdata.electric.egmX(iPoint,:);
+    iPoint = inpolyhedron(FV, QPTS, 'tol', tol);
+end
 
 end
