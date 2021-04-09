85182e9f7d3ef504111076fe2ed266757a471b1b
Steven Williams <steven.e.williams@kcl.ac.uk>
2021-04-09 14:20:24 +0100

    Finished creating an initial version of openEPDataInterpolator.m

--- openEpDataInterpolator.m
+++ openEpDataInterpolator.m
@@ -3,10 +3,14 @@ classdef openEpDataInterpolator
     
     properties
         method
+    end
+    
+    properties (Access = private)
         interMethod
         exterMethod
         distanceThreshold
         smoothingLength
+        fillWith
     end
     
     methods
@@ -18,6 +22,8 @@ classdef openEpDataInterpolator
             int.exterMethod = 'linear';
             int.distanceThreshold = Inf;
             int.smoothingLength = 10;
+            int.fillWith = NaN;
+            
             int.method = 'scatteredinterpolant';
             
             if nargin==1
@@ -27,17 +33,39 @@ classdef openEpDataInterpolator
                 options = varargin{2};
                 if isfield(options, 'interMethod')
                     if ~isempty(options.interMethod)
-                        int.interMethod = options.interMethod;
+                        if ~strcmpi(int.method, 'scatteredInterpolant')
+                            warning(['OPENEP/OPENEPDATAINTERPOLATOR: Setting interMethod property has no effect for interpolation method ' int.method]);
+                            int.interMethod = options.interMethod;
+                        end
                     end
                 end
                 if isfield(options, 'exterMethod')
                     if ~isempty(options.exterMethod)
-                        int.exterMethod = options.exterMethod;
+                        if ~strcmpi(int.method, 'scatteredInterpolant')
+                            warning(['OPENEP/OPENEPDATAINTERPOLATOR: Setting exterMethod property has no effect for interpolation method ' int.method]);
+                            int.exterMethod = options.exterMethod;
+                        end
                     end
                 end
                 if isfield(options, 'distanceThreshold')
                     if ~isempty(options.distanceThreshold)
                         int.distanceThreshold = options.distanceThreshold;
+                    end
+                end
+                if isfield(options, 'smoothingLength')
+                    if ~isempty(options.smoothingLength)
+                        if ~strcmpi(int.method, 'localSmoothing')
+                            warning(['OPENEP/OPENEPDATAINTERPOLATOR: Setting fillWith property has no effect for interpolation method ' int.method])
+                            int.smoothingLength = options.smoothingLength;
+                        end
+                    end
+                end
+                if isfield(options, 'fillWith')
+                    if ~isempty(options.fillWith)
+                        if ~strcmpi(int.method, 'localSmoothing')
+                            warning(['OPENEP/OPENEPDATAINTERPOLATOR: Setting fillWith property has no effect for interpolation method ' int.method])
+                            int.fillWith = options.fillWith;
+                        end
                     end
                 end
             end
@@ -58,16 +86,30 @@ classdef openEpDataInterpolator
             % perform interpolation
             switch lower(int.method)
                 case 'scatteredinterpolant'
+                    
+                    % error checking
                     if numel(d0) < 4
                         error('OPENEP/OPENEPDATAINTERPOLATOR: Cannot perform interpolation with less than four data points');
                     end
-                    F = scatteredInterpolant(x0(:,1), x0(:,2), x0(:,3) ...
-                        , d0, int.interMethod, int.exterMethod);
+                    
+                    % interpolation
+                    F = scatteredInterpolant(x0(:,1), x0(:,2), x0(:,3), d0, int.interMethod, int.exterMethod);
                     d1 = F(x1);
                     
                 case 'localsmoothing'
                     
+                    % error checking
+                         % % % AC: is there any appropriate error checking to do
+                    % here?
+                    
+                    % interpolation
+                    d1 = localSmoothing(x0, d0, x1, int.smoothingLength, int.fillWith);
+                    
                 case 'radialbasis'
+                    
+                    % error checking
+                    
+                    % interpolation
             end
             
             % remove interpolated data that is too far from real data
---
+++ private/localSmoothing.m
@@ -0,0 +1,68 @@
+function [f_dash, df_dash] = localSmoothing(x, f, x_dash, smoothingLength, fillWith)
+% ------------
+% AC 8/2/21: local smoothing or piecewise interpolation, and locally
+% centered moving least-squares gradient
+% ------------
+% x is the points at which field values f are defined. 
+%   size(x) = [m, 3], where m is the number of points.
+% f are the values at the points x. size(f) = [m, 1].
+% x_dash are the points at which to query f. size(x_dash) = [n, 3].
+% smoothingLength is a scalar smoothing length. 
+%   size(smoothingLength) = [1, 1]
+% f_dash is the estimated function at points x_dash. size(f_dash) =
+% [n, 1]
+% df_dash is the estimted function gradient at x_dash. size(df_dash) = [n,
+% 3]
+% ------------
+
+f_dash = zeros(length(x_dash), 1);
+df_dash = zeros(length(x_dash), 3);
+[Idx, dists] = rangesearch(x, x_dash, smoothingLength);
+no_smooth = cellfun(@isempty, Idx);
+if strcmp(fillWith, 'nearestneighbour')
+    nnIdx = knnsearch(x, x_dash(no_smooth, :));
+    f_dash(no_smooth) = f(nnIdx);
+else
+    f_dash(no_smooth) = NaN;
+end
+
+kern = @(x, h) exp(-(x./h).^2);
+% kern = @cubicSpline;
+
+for i=1:numel(Idx)
+    
+    if ~isempty(Idx{i})
+        
+        dx = x(Idx{i},:) - x_dash(i, :);
+        
+        wi = kern(dists{i}', smoothingLength);
+        psi = wi./sum(wi);
+        
+        Xi = zeros(3);
+        for j = 1:size(dx, 1)
+            Xi = Xi + psi(j)*dx(j, :)'*dx(j, :);
+        end
+        bi = pinv(Xi);
+        
+        f_dash(i) = sum(f(Idx{i}).*psi);
+        
+        for j=1:size(dx, 1)
+            df_dash(i, :) = df_dash(i, :) + (f(Idx{i}(j)) - f_dash(i))*dx(j,:)*bi*psi(j);
+        end
+        
+    end
+    
+end
+
+
+    function w = cubicSpline(d, smoothingLength)
+        q = d./smoothingLength;
+        d1 = 1 - 1.5*q.^2.*(1 - q./2);
+        d2 = 0.25*(2 - q).^3;
+        w = zeros(size(d));
+        w(q<=1) = d1(q<=1);
+        w(q>1 & q<=2) = d2(q>1 & q<=2);
+    end
+
+
+end
