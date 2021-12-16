e6816fe95a436640a3423b3859e6389cf3588843
Paul Smith <paul.smith@kcl.ac.uk>
2021-12-16 11:46:05 +0000

    Remove git merge conflict lines

--- getMesh.m
+++ getMesh.m
@@ -10,12 +10,9 @@ function tr = getMesh(userdata, varargin)
 %   'type'     {'trirep'}|'triangulation'
 %       - Specifies whether to return the mesh as a TriRep object or as a
 %       Triangulation object
-<<<<<<< HEAD
 %   'limitToTriangulation' {'false'}|true
 %       - Specifies whether to repack the triangulation
-=======
 %   'repack'    {false}|true
->>>>>>> origin/develop
 %
 % GETMESH Returns a face/vertex representation of the anatomical model. 
 % Supported data types include istances of the Matlab objects Trirep and 
@@ -37,23 +34,18 @@ function tr = getMesh(userdata, varargin)
 
 nStandardArgs = 1; % UPDATE VALUE
 type = 'trirep';
-<<<<<<< HEAD
 limitToTriangulation = false;
-=======
 dorepack = false;
->>>>>>> origin/develop
+
 if nargin > nStandardArgs
     for i = 1:2:nargin-nStandardArgs
         switch lower(varargin{i})
             case 'type'
                 type = varargin{i+1};
-<<<<<<< HEAD
             case 'limittotriangulation'
                 limitToTriangulation - varargin{i+1};
-=======
             case 'repack'
                 dorepack = varargin{i+1};
->>>>>>> origin/develop
         end
     end
 end
@@ -61,21 +53,16 @@ if ~any(strcmpi({'trirep' 'triangulation'}, type))
     error(['OPENEP/GETMESH: Value: ' type ' for parameter: type not recognised']);
 end
 
-<<<<<<< HEAD
-=======
 % Check the type of surface data stored in the OpenEP datastructure and
 % accordingly access the triangulation and the points
->>>>>>> origin/develop
 if isa(userdata.surface.triRep, 'TriRep')
     FV.vert = userdata.surface.triRep.X;
     FV.faces = userdata.surface.triRep.Triangulation;
 elseif isa(userdata.surface.triRep, 'triangulation')
     FV.vert = userdata.surface.triRep.Points;
     FV.faces = userdata.surface.triRep.ConnectivityList;
-<<<<<<< HEAD
 else
     error('OPENEP/getMesh: userdata.surface.TriRep should be a TriRep or a triangulation object')
-=======
 elseif isa(userdata.surface.triRep, 'struct')
     if ~isfield(userdata.surface.triRep, 'X')
         error('OPENEP/GETMESH: invalid data. userdata.surface.triRep must be one of: TriRep, triangulation or struct with fields .X and .Triangulation');
@@ -85,7 +72,6 @@ elseif isa(userdata.surface.triRep, 'struct')
     end
     FV.vert = userdata.surface.triRep.X;
     FV.faces = userdata.surface.triRep.Triangulation;
->>>>>>> origin/develop
 end
 
 switch lower(type)
@@ -103,13 +89,10 @@ switch lower(type)
         end
 end
 
-<<<<<<< HEAD
 if limitToTriangulation
     tr = repack(tr);
 end
 
-=======
 if dorepack
     tr = repack(tr);
->>>>>>> origin/develop
 end
