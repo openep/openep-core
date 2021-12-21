189be4e737ceecab701a0dfc225ef46f32321cd8
Paul Smith <paul.smith@kcl.ac.uk>
2021-12-21 12:38:29 +0000

    Load dataset at class setup rather than for each test

--- tests/getVolumeTest.m
+++ tests/getVolumeTest.m
@@ -1,9 +1,13 @@
 classdef getVolumeTest < matlab.unittest.TestCase
+    methods(TestClassSetup)
+        function loadData()
+            load 'openep_dataset_2.mat' dataset_2;
+        end
+    end
     methods(Test)
         function dataset2Volume(testCase)
-            load 'openep_dataset_2.mat' userdata;
             expected_volume = 173.8810;
-            calculated_volume = getVolume(userdata);
+            calculated_volume = getVolume(dataset_2);
             testCase.verifyEqual(expected_volume,calculated_volume)
         end
     end
